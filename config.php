01807fb7da74e9c1eee4a5fca22a2e7893bc3bd7
root <root@ip-10-60-11-46.eu-west-1.compute.internal>
2021-06-03 13:10:16 +0200

    modificacio per entorn AWS

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 unset($CFG);
 global $CFG;
 $CFG = new stdClass();
-require_once(__DIR__ . '/../config-moodle2.php');
+require_once(__DIR__ . '/../../config-moodle2.php');
 require_once(__DIR__ . '/lib/setup.php');
