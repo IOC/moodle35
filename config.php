902a3d4d663ecd3d7b5d2a6c5105c829630fe426
Ubuntu <ubuntu@ip-10-60-6-37.eu-west-1.compute.internal>
2021-07-14 10:24:12 +0200

    New config file

--- config.php
+++ config.php
@@ -1,32 +1,7 @@
 <?php
+
 unset($CFG);
 global $CFG;
 $CFG = new stdClass();
-
-$CFG->dbtype    = 'mysqli';
-$CFG->dblibrary = 'native';
-$CFG->dbhost    = '10.6.0.5';
-$CFG->dbname    = 'moodle';
-$CFG->dbuser    = 'root';
-$CFG->dbpass    = '123456';
-$CFG->prefix    = 'mdl_';
-$CFG->dboptions = array (
-  'dbpersist' => 0,
-  'dbport' => 3306,
-  'dbsocket' => '',
-  'dbcollation' => 'utf8mb4_unicode_ci',
-);
-
-$CFG->wwwroot   = 'https://campus.ticxcat';
-$CFG->dataroot  = '//moodledata';
-$CFG->admin     = 'admin';
-
-$CFG->directorypermissions = 0777;
-
-$CFG->session_handler_class = '\core\session\file';
-$CFG->session_file_save_path = $CFG->dataroot.'/sessions';
-
-// There is no php closing tag in this file,
-// it is intentional because it prevents trailing whitespace problems!
-
+require_once(__DIR__ . '/../../config-moodle2.php');
 require_once(__DIR__ . '/lib/setup.php');
