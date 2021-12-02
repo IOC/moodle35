b14b3d6e69f2bcac13a3982881879e06d2dbcf8a
Israel Forés <israel.fores@ticxcat.cat>
2021-12-02 09:57:42 +0100

    Treiem el block mycourses

--- blocks/mycourses/lang/ca/block_mycourses.php
+++
@@ -1,8 +0,0 @@
-<?php
-$string['pluginname'] = 'Els meus cursos';
-$string['mycoursesperpage'] = 'Limita el nombre de cursos que es mostraran';
-$string['mycoursesoverview'] = 'Oculta el resum d\'activitats si el temps mig de resposta del servidor supera el temps indicat';
-$string['mycourses:addinstance'] = 'Afegeix un nou bloc Els meus cursos';
-$string['mycourses:myaddinstance'] = 'Afegeix un nou bloc Els meus cursos a La meva pàgina inicial';
-$string['overload'] = 'No es mostra el resum de les activitats per l\'alt nivell de càrrega del servidor';
-$string['privacy:metadata'] = 'Els meus cursos no enregistra cap dada personal.';
--- blocks/mycourses/lang/en/block_mycourses.php
+++
@@ -1,8 +0,0 @@
-<?php
-$string['pluginname'] = 'My courses';
-$string['mycoursesperpage'] = 'Limit the number of courses showing up';
-$string['mycoursesoverview'] = 'Hide overview when average server access time its above time specified';
-$string['mycourses:addinstance'] = 'Add a new My courses block';
-$string['mycourses:myaddinstance'] = 'Add a new My courses block to My home';
-$string['overload'] = 'Due server overload, overview is not avaliable';
-$string['privacy:metadata'] = 'My courses plugin does not store any personal data.';
