80d740a312ada85b0c3825bbcf24e0de2dba541b
Israel Forés <israel.fores@ticxcat.cat>
2022-03-24 12:33:30 +0100

    Update mod questionnaire

--- mod/questionnaire/version.php
+++ mod/questionnaire/version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2020111100;  // The current module version (Date: YYYYMMDDXX).
-$plugin->requires = 2020110900; // Moodle version (3.10).
+$plugin->version  = 2020111102;  // The current module version (Date: YYYYMMDDXX).
+$plugin->requires = 2020061500; // Moodle version (3.9).
 
 $plugin->component = 'mod_questionnaire';
 
-$plugin->release  = '3.10.0 (Build - 2020111801)';
+$plugin->release  = '3.10.2 (Build - 2022030300)';
 $plugin->maturity  = MATURITY_STABLE;
