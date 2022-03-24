d407cb7c1dbab6803ed250d3055c9244530f5c04
Israel Forés <israel.fores@ticxcat.cat>
2022-03-24 12:38:05 +0100

    Update mod videotime

--- mod/videotime/classes/local/tabs/tab.php
+++ mod/videotime/classes/local/tabs/tab.php
@@ -158,7 +158,7 @@ abstract class tab {
      */
     public static function add_form_fields($mform) {
         $name = preg_replace('/^videotimetab_(.*)\\\\tab/', '$1', get_called_class());
-        if (empty(get_config("videotimetab_$name", 'enabled'))) {
+        if (empty(get_config("videotimetab_$name", 'enabled')) || !empty(self::added_dependencies())) {
             return;
         }
 
@@ -209,7 +209,7 @@ abstract class tab {
      */
     public function is_enabled(): bool {
         $name = preg_replace('/^videotimetab_(.*)\\\\tab/', '$1', get_called_class());
-        return !empty(get_config("videotimetab_$name", 'enabled'));
+        return !empty(get_config("videotimetab_$name", 'enabled')) && empty(self::added_dependencies());
     }
 
     /**
@@ -226,11 +226,26 @@ abstract class tab {
      *
      * @return bool
      */
-    protected function get_record(): bool {
+    protected function get_record(): stdClass {
+        global $DB;
+
         $name = preg_replace('/^videotimetab_(.*)\\\\tab/', '$1', get_called_class());
         if (is_null($this->record)) {
-            $this->record = $DB->get_record("videotimetab_$name", array('videotime' => $instance->id));
+            $this->record = $DB->get_record("videotimetab_$name", array('videotime' => $this->get_instance()->id));
         }
         return $this->record;
+    }
+
+    /**
+     * Hook to set up page by adding blocks etc.
+     */
+    public function setup_page() {
+    }
+
+    /**
+     * List of missing dependencies needed for plugin to be enabled
+     */
+    public static function added_dependencies() {
+        return '';
     }
 }
