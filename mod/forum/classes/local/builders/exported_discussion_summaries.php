c294695a519844c86f37418e880132e2c36060bc
Israel Forés <israel.fores@ticxcat.cat>
2022-04-25 08:24:08 +0200

    Added patch IOC046: Show the number of attachments in each discussion

--- mod/forum/classes/local/builders/exported_discussion_summaries.php
+++ mod/forum/classes/local/builders/exported_discussion_summaries.php
@@ -193,6 +193,26 @@ class exported_discussion_summaries {
         }
         // Fi.
 
+        // @PATCH IOC046: Show the number of attachments in each discussion.
+        $getiddiscussions = static function($discussion) {
+            return $discussion->id;
+        };
+
+        $discussionsids = array_map($getiddiscussions, $exportedposts['summaries']);
+        $attachments = $this->forum_get_discussion_num_attachments($forumid, $context->id, $discussionsids);
+
+        global $OUTPUT;
+
+        foreach ($exportedposts['summaries'] as $key => $record) {
+            $exportedposts['summaries'][$key]->discussion->title = $exportedposts['summaries'][$key]->discussion->name;
+            if (isset($attachments[$record->id])) {
+                $src = $OUTPUT->image_url('t/attachment', 'mod_forum');
+                $title = get_string('overviewnumattachments', 'forum', $attachments[$record->id]->num);
+                $exportedposts['summaries'][$key]->discussion->name .= "&nbsp;<img src=\"$src\" title=\"$title\" />";
+            }
+        }
+        // Fi.
+
         // Pass the current, preferred sort order for the discussions list.
         $discussionlistvault = $this->vaultfactory->get_discussions_in_forum_vault();
         $sortorder = get_user_preferences('forum_discussionlistsortorder',
@@ -320,4 +340,53 @@ class exported_discussion_summaries {
         $authorvault = $this->vaultfactory->get_author_vault();
         return $authorvault->get_context_ids_for_author_ids($authorids);
     }
+
+    // @PATCH IOC046: Show the number of attachments in each discussion.
+    /**
+     * Count the number of attachments in each discussion
+     *
+     * @param int $forumid
+     * @param int $contextid
+     * @param array $discussions
+     * @return array
+     * @throws \coding_exception
+     * @throws \dml_exception
+     */
+    private function forum_get_discussion_num_attachments(int $forumid, int $contextid, array $discussions = []): array {
+
+        global $DB;
+
+        if (empty($discussions)) {
+            return [];
+        }
+
+        [$attachsql, $attachparams] = $DB->get_in_or_equal($discussions, SQL_PARAMS_NAMED);
+
+        $sql = "SELECT fd.id, COUNT(f.id) as num
+            FROM {forum_discussions} as fd
+            JOIN {forum_posts} as fp ON fp.discussion=fd.id
+            JOIN {files} as f ON f.itemid=fp.id
+            WHERE fd.forum = :forum
+            AND f.component = :component
+            AND (f.filearea = :filearea1 OR f.filearea = :filearea2)
+            AND f.contextid = :contextid
+            AND f.filename != :filename
+            AND fd.id $attachsql
+            GROUP BY fd.id";
+
+        $params = [
+            'forum' => $forumid,
+            'component' => 'mod_forum',
+            'filearea1' => 'attachment',
+            'filearea2' => 'post',
+            'contextid' => $contextid,
+            'filename' => '.',
+        ];
+
+        $params = array_merge($params, $attachparams);
+
+        return $DB->get_records_sql($sql, $params);
+    }
+    // Fi.
+
 }
