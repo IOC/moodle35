4f8f2ab83f883cbc5b5852c99460fd218f65c21f
Toni Ginard <toni.ginard@gmail.com>
2022-07-21 13:10:58 +0200

    Forced value of loglifetime to 1000 days

--- config.php
+++ config.php
@@ -4,6 +4,14 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 require_once(__DIR__ . '/../../config-moodle2.php');
+
+$CFG->forced_plugin_settings = [
+    'logstore_standard' => [
+        'loglifetime' => 1000,
+    ],
+];
+
 require_once __DIR__ . '/local/userdebug/lib.php';
 userdebug_get_debug();
+
 require_once(__DIR__ . '/lib/setup.php');
